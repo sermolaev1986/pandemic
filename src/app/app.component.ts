785c6556232da5d1a27dd9ba10dd85293bbd8d79
Sergei Ermolaev <sergei.ermolaev@magenta.at>
2020-05-03 15:59:50 +0200

    first scratch of worldmap with cities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {keyframes} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-test-new';
+  citiesMap = new Map<string, City>();
+  cities: City[] = [];
+
+  constructor(private http: HttpClient) {
+    this.http = http;
+  }
+
+  ngOnInit() {
+    this.getJSON().subscribe(data => {
+      this.citiesMap = data;
+      const citiesMap = this.citiesMap;
+      this.cities = Array.from(Object.keys(citiesMap).map(key => citiesMap[key]));
+    });
+  }
+
+  public getJSON(): Observable<any> {
+    return this.http.get('./assets/cities.json');
+  }
+
+}
+
+class City {
+  name: string;
+  top: number;
+  left: number;
+  color: string;
+  connectedCities: string[];
+
+  constructor(name: string, top: number, left: number, connectedCities: string[]) {
+    this.name = name;
+    this.top = top;
+    this.left = left;
+    this.connectedCities = connectedCities;
+  }
 }
